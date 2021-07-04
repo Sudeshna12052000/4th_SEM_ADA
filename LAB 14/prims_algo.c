97c6b4da310e372bf1fe4e3f17dbdc81e8b32e77
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-07-04 14:43:59 +0530

    Update prims_algo.c

--- LAB 14/prims_algo.c
+++ LAB 14/prims_algo.c
@@ -1,58 +1,56 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <limits.h>
-
-int n;
-
-int minWeight(int weight[], bool included[])
-{
-    int min = INT_MAX, index = -1;
-    for(int i = 0; i < n; i++)
-    {
-        if(!included[i] && weight[i] < min)
-            min = weight[i], index = i;
-    }
-    return index;
-}
-
-void mst(int graph[n][n])
-{
-    int parent[n], weight[n];
-    bool included[n];
-    for(int i = 0; i < n; i++)
-        weight[i] = INT_MAX, included[i] = false;
-    weight[0] = 0;
-    parent[0] = -1;
-
-    for(int i = 0; i < n-1; i++)
-    {
-        int k = minWeight(weight, included);
-        included[k] = true;
-        for(int j = 0; j < n; j++)
-        {
-            if(graph[i][j] && !included[j] &&  graph[i][j] < weight[j])
-                parent[j] = i, weight[j] = graph[i][j];
-        }
-    }
-
-    printf("Edge \tWeight\n");
-    for (int i = 1; i < n; i++)
-        printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
-}
-
-int main()
-{
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-
-    scanf("%d",&n); // Number of nodes
-
-    int graph[n][n];
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-            scanf("%d",&graph[i][j]);
-    }
-    mst(graph);
-    return 0;
-}
+#include <stdio.h>  
+#include <limits.h>  
+#define vertices 5  
+  
+int minimum_key(int k[], int mst[])  
+{  
+   int minimum  = INT_MAX, min,i;  
+   
+   for (i = 0; i < vertices; i++)  
+     if (mst[i] == 0 && k[i] < minimum )  
+         minimum  = k[i], min = i;  
+   
+   return min;  
+}  
+  
+   
+void prim(int g[vertices][vertices])  
+{  
+     int parent[vertices];   
+     int k[vertices];     
+     int mst[vertices];    
+     int i, count,u,v;   
+     for (i = 0; i < vertices; i++)  
+        k[i] = INT_MAX, mst[i] = 0;  
+   
+     k[0] = 0;       
+     parent[0] = -1;   
+   
+     for (count = 0; count < vertices-1; count++)  
+     {  
+     
+        u = minimum_key(k, mst);  
+      mst[u] = 1;  
+   
+        for (v = 0; v < vertices; v++)  
+   
+          if (g[u][v] && mst[v] == 0 && g[u][v] <  k[v])  
+             parent[v]  = u, k[v] = g[u][v];  
+     }  
+   
+    for (i = 1; i < vertices; i++)  
+      printf("%d  %d    %d \n", parent[i], i, g[i][parent[i]]);  
+}  
+   
+   
+void main()  
+{  
+   int g[vertices][vertices] = {{3, 2, 1, 9, 0},  
+                      {5, 1, 2, 10, 4},  
+                      {0, 4, 1, 0, 9},  
+                      {8, 10, 0, 2, 10},  
+                      {1, 6, 8, 11, 0},  
+                     };  
+   
+    prim(g);  
+}  
