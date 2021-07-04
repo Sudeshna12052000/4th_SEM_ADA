514e95b4156f390cf65fc1a63872e0498459c3e1
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-07-04 21:28:06 +0530

    Update nqueens.c

--- LAB 18/nqueens.c
+++ LAB 18/nqueens.c
@@ -1,66 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <stdbool.h>
+#define N 4 
+#include <stdbool.h> 
+#include <stdio.h> 
+void printSolution(int board[N][N]) 
+{ 
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < N; j++) 
+            printf(" %d ", board[i][j]); 
+        printf("\n"); 
+    } 
+} 
 
-int n;
-
-bool isSafe(int board[n][n], int row, int col)
-{
-    for(int i = 0; i < n; i++)
-        if(board[row][i])
+bool isSafe(int board[N][N], int row, int col) 
+{ 
+    int i, j; 
+    for (i = 0; i < col; i++) 
+        if (board[row][i]) 
+            return false; 
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+        if (board[i][j]) 
             return false;
-    
-    for(int i = row, j = col; i >= 0 && j >= 0; i--, j--)
-        if(board[i][j])
-            return false;
-
-    for (int i = row, j = col; j >= 0 && i < n; i++, j--)
-        if (board[i][j])
-            return false;
+    for (i = row, j = col; j >= 0 && i < N; i++, j--) 
+        if (board[i][j]) 
+            return false; 
   
-    return true;
-}
-
-bool solve(int board[n][n], int col)
-{
-    if(col >= n)
+    return true; 
+} 
+bool solveNQUtil(int board[N][N], int col) 
+{ 
+    if (col >= N) 
         return true;
-    
-    for(int i = 0; i < n; i++)
-    {
-        if(isSafe(board, i, col))
-        {
-            board[i][col] = 1;
-            if(solve(board, col+1))
-                return true;
-            board[i][col] = 0;
-        }
-    }
-    return false;
-}
+    for (int i = 0; i < N; i++) 
+    { 
+        if (isSafe(board, i, col)) 
+        { 
+            board[i][col] = 1; 
+            if (solveNQUtil(board, col + 1)) 
+                return true; 
+            board[i][col] = 0; 
+        } 
+    } 
+    return false; 
+} 
+bool solveNQ() 
+{ 
+    int board[N][N] = { { 0, 0, 0, 0 }, 
+                        { 0, 0, 0, 0 }, 
+                        { 0, 0, 0, 0 }, 
+                        { 0, 0, 0, 0 } }; 
+  
+    if (solveNQUtil(board, 0) == false) { 
+        printf("Solution does not exist"); 
+        return false; 
+    } 
+  
+    printSolution(board); 
+    return true; 
+} 
 
-int main()
-{
-    freopen("output.txt", "w", stdout);
-    n = 8;
-    int board[n][n];
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-            board[i][j] = 0;
-    }
-    if (solve(board, 0) == false) {
-        printf("Solution does not exist");
-    }
-    else
-    {
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-                printf("%d ", board[i][j]);
-            printf("\n");
-        }
-    }
-    return 0;
-}
+int main() 
+{ 
+    solveNQ(); 
+    return 0; 
+} 
